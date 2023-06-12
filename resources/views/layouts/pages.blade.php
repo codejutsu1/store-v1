b8c34f3f31e9fb0c30f12f5bb1626d0df5019083
codejutsu1 <codejutsu@protonmail.com>
2023-06-12 17:11:54 +0100

    Worked on the front page of the website

---
+++ resources/views/layouts/pages.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Under construction / E-commerce Website</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        <nav class="bg-zinc-800 flex justify-around items-center text-gray-200 py-3">
+            <div>
+                <h1>Logo Here...</h1>
+            </div>
+            <ul class="flex items-center space-x-20">
+                <li>
+                    <a href="#">
+                        Category 1
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Category 2
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Category 3
+                    </a>
+                </li>
+            </ul>
+            <div class="flex items-center space-x-5">
+                <input type="search" name="Search Here" id="search" class="w-20 h-3">
+
+                <div>
+                    <h1>Cart (0)</h1>
+                </div>
+            </div>
+        </nav>
+        <div class="py-2">
+            <p class="text-center font-semibold">Important Announcement, all products will have a discount of <span class="font-bold">30%</span>, valid till 30th August.</p>
+        </div>
+        <main>
+            {{ $slot }}
+        </main>
+    </body>
+</html>
