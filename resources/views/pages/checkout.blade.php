0ac0082a857a3ac47ec810d8985b213a774a28d1
codejutsu1 <codejutsu@protonmail.com>
2023-06-27 03:51:38 +0100

    completely made the checkout page responsive

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -1,29 +1,29 @@
 <x-page-layout>
     <section class="bg-[#101820]">
         <div class="w-5/6 mx-auto py-5 flex justify-center">
-            <p class="text-2xl font-semibold text-gray-200 uppercase">
-            <span class="underline"><a href="{{ route('home') }}">Home</a></span> / Cart / <span class="inline-block text-xl text-[#FEE715]"><i>Checkout</i></span> 
+            <p class="text-xl md:text-2xl font-semibold text-gray-200 uppercase">
+            <span class="underline"><a href="{{ route('home') }}">Home</a></span> / Cart / <span class="inline-block text-lg md:text-xl text-[#FEE715]"><i>Checkout</i></span> 
             </p><br>
         </div>
         <a href="{{ route('shop') }}" class="text-[#FEE715] underline block text-center pb-5">Continue shopping?</a>
     </section>
 
     <section>
-        <div class="w-5/6 mx-auto flex justify-between gap-10">
-            <div class="py-5 w-3/5"> 
-                <h1 class="text-2xl font-semibold text-center uppercase">Billing Details</h1>
+        <div class="w-11/12 md:w-5/6 mx-auto md:flex justify-between gap-10">
+            <div class="py-5 md:w-3/5 w-full"> 
+                <h1 class="text-xl md:text-2xl font-semibold text-center uppercase">Billing Details</h1>
                 <form action="#" class="space-y-10 py-5">
-                    <div class="flex justify-between gap-5">
-                        <input type="text" placeholder="First Name *" class="rounded-md p-3 block w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
-                        <input type="text" placeholder="Last Name *" class="rounded-md p-3 block w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
+                    <div class="md:flex justify-between md:gap-5 space-y-10 md:space-y-0">
+                        <input type="text" placeholder="First Name *" class="rounded-md p-3 block w-full md:w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
+                        <input type="text" placeholder="Last Name *" class="rounded-md p-3 block w-full md:w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
                     </div>
-                    <div class="flex justify-between gap-5">
-                        <input type="text" placeholder="Email *" class="rounded-md p-3 block w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
-                        <input type="text" placeholder="Phone Number *" class="rounded-md p-3 block w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
+                    <div class="md:flex justify-between md:gap-5 space-y-10 md:space-y-0">
+                        <input type="text" placeholder="Email *" class="rounded-md p-3 block w-full md:w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
+                        <input type="tel" placeholder="Phone Number *" class="rounded-md p-3 block w-full md:w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
                     </div>
-                    <div class="flex justify-between gap-5">
-                        <input type="text" placeholder="Lodge *" class="rounded-md p-3 block w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
-                        <select name="area" id="area" class="rounded-md p-3 block w-1/2 focus:ring-0 focus:border-[#101820] text-gray-500 border-gray-300">
+                    <div class="md:flex justify-between md:gap-5 space-y-10 md:space-y-0">
+                        <input type="text" placeholder="Lodge *" class="rounded-md p-3 block w-full md:w-1/2 focus:ring-0 focus:border-[#101820] border-gray-300">
+                        <select name="area" id="area" class="rounded-md p-3 block w-full md:w-1/2 focus:ring-0 focus:border-[#101820] text-gray-500 border-gray-300">
                             <option disabled selected>Eziobodo/Ihagwa/Umuchima/Obinze?</option>
                             <option value="eziobodo">Eziobodo</option>
                             <option value="umuchima">Umuchima</option>
@@ -37,7 +37,7 @@
                 </form>
             </div>
 
-            <div class="py-5 w-2/5">
+            <div class="py-5 md:w-2/5 w-full">
                 <div class="px-4 space-y-8 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20 shadow-lg border-t">
                     <h1 class="text-2xl font-semibold text-center border-b border-[#FEE715] py-3">Order Summary</h1>
                     <div class="flex justify-between items-center border-b border-[#FEE715] pb-3">
