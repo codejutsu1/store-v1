7e5ba5af4d85a575c96aff645add2cdba3fddd7d
codejutsu1 <codejutsu@protonmail.com>
2023-06-27 03:02:15 +0100

    Worked on the responsive about us page

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,17 +1,17 @@
 <x-page-layout>
     <section class="bg-[#101820]">
         <div class="w-5/6 mx-auto pb-10">
-             <h1 class="py-10 text-center text-4xl font-bold uppercase text-[#FEE715]">Meet Our Oga</h1>
+             <h1 class="py-10 text-center text-2xl md:text-4xl font-bold uppercase text-[#FEE715]">Meet Our Oga</h1>
             <div>
-                <div class="mx-auto shadow-custom py-20 w-1/3 grayscale duration-300 hover:grayscale-0 cursor-pointer">
+                <div class="mx-auto shadow-custom py-20 w-5/6 md:w-1/3 grayscale duration-300 hover:grayscale-0 cursor-pointer">
                     <div class="flex justify-center items-center">
                         <img class="w-56 h-56 rounded-full border-2 border-[#FEE715] object-cover" src="{{ Vite::asset('resources/images/review/human-3.jpg') }}" alt="An Image of a man.">
                     </div>
                     <div class="py-4">
-                        <h1 class="text-center text-gray-200 text-2xl font-semibold">
+                        <h1 class="text-center text-gray-200 text-xl md:text-2xl font-semibold">
                             Gerald Gius
                         </h1>
-                        <span class="text-center text-xl text-gray-300 block font-semibold">(D General)</span>
+                        <span class="text-center text-lg md:text-xl text-gray-300 block font-semibold">(D General)</span>
                         <div class="w-3/5 flex justify-between mx-auto pt-4">
                             <a href="#">
                                 <img class="w-6 object-cover" src="{{ Vite::asset('resources/images/review/twitter.svg') }}" alt="reviewer tiktok">
@@ -37,16 +37,16 @@
     <x-feed-backs />
 
     <section id="faq">
-        <div class="w-3/5 mx-auto py-20">
-            <h1 class="text-4xl font-bold mb-10">Frequently asked questions?</h1>
+        <div class="w-11/12 md:w-3/5 mx-auto py-20">
+            <h1 class="text-2xl md:text-4xl font-bold mb-10">Frequently asked questions?</h1>
 
             <div class="flex flex-col space-y-10">
                 <div class="p-4 shadow-lg rounded-md tab-acc relative">
                     <input type="radio" id="acc1" name="acc" class="hidden">
                     <label for="acc1" class="label-acc">
-                        <h1 class="text-2xl font-bold text-[#101820]">Who are we?</h1>
+                        <h1 class="text-xl md:text-2xl font-bold text-[#101820]">Who are we?</h1>
                     </label>
-                    <div class="text-gray-500 text-lg content-acc max-h-0 overflow-hidden">
+                    <div class="text-gray-500 text-base md:text-lg content-acc max-h-0 overflow-hidden">
                         <p class="py-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ex quisquam, modi amet reiciendis ut molestias! Ipsam, fuga illo. Error quam saepe consectetur facere qui sapiente veritatis assumenda dolorem earum.</p>
                     </div>
                 </div>
@@ -54,9 +54,9 @@
                 <div class="p-4 shadow-lg rounded-md tab-acc relative">
                     <input type="radio" id="acc2" name="acc" class="hidden">
                     <label for="acc2" class="label-acc">
-                        <h1 class="text-2xl font-bold text-[#101820]">How do we deliver?</h1>
+                        <h1 class="text-xl md:text-2xl font-bold text-[#101820]">How do we deliver?</h1>
                     </label>
-                    <div class="text-gray-500 text-lg content-acc max-h-0 overflow-hidden">
+                    <div class="text-gray-500 text-base md:text-lg content-acc max-h-0 overflow-hidden">
                         <p class="py-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ex quisquam, modi amet reiciendis ut molestias! Ipsam, fuga illo. Error quam saepe consectetur facere qui sapiente veritatis assumenda dolorem earum.</p>
                     </div>
                 </div>
@@ -64,10 +64,10 @@
                 <div class="p-4 shadow-lg rounded-md tab-acc relative">
                     <input type="radio" id="acc3" name="acc" class="hidden">
                     <label for="acc3" class="label-acc">
-                        <h1 class="text-2xl font-bold text-[#101820]">When is the cookathon coming to pass?</h1>
+                        <h1 class="text-xl md:text-2xl font-bold text-[#101820]">When is the cookathon coming to pass?</h1>
                     </label>
-                    <div class="text-gray-500 text-lg content-acc max-h-0 overflow-hidden">
-                        <p class="py-10 font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ex quisquam, modi amet reiciendis ut molestias! Ipsam, fuga illo. Error quam saepe consectetur facere qui sapiente veritatis assumenda dolorem earum.</p>
+                    <div class="text-gray-500 text-base md:text-lg content-acc max-h-0 overflow-hidden">
+                        <p class="py-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ex quisquam, modi amet reiciendis ut molestias! Ipsam, fuga illo. Error quam saepe consectetur facere qui sapiente veritatis assumenda dolorem earum.</p>
                     </div>
                 </div>
             </div>
