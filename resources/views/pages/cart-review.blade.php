1766c16f6e9fdfcde5806692b3a3afb45114edcd
codejutsu1 <codejutsu@protonmail.com>
2023-06-27 03:41:07 +0100

    worked on the cart-review page, will make my table overflow as soon as possible

--- resources/views/pages/cart-review.blade.php
+++ resources/views/pages/cart-review.blade.php
@@ -1,17 +1,17 @@
 <x-page-layout>
     <section class="bg-[#101820]">
         <div class="w-5/6 mx-auto py-5 flex justify-center">
-            <p class="text-2xl font-semibold text-gray-200 uppercase">
-                <span class="underline"><a href="{{ route('home') }}">Home</a></span> / <span class="inline-block text-xl text-[#FEE715]"><i>Review Cart</i></span> 
+            <p class="text-xl md:text-2xl font-semibold text-gray-200 uppercase">
+                <span class="underline"><a href="{{ route('home') }}">Home</a></span> / <span class="inline-block text-lg md:text-xl text-[#FEE715]"><i>Review Cart</i></span> 
             </p><br>
         </div>
         <a href="{{ route('shop') }}" class="text-[#FEE715] underline block text-center pb-5">Continue shopping?</a>
     </section>
 
     <section>
-        <div class="w-5/6 mx-auto py-10">
-            <div class="border p-4 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20 shadow-xl">
-                <table class="w-full"> 
+        <div class="w-11/12 md:w-5/6 mx-auto py-10">
+            <div class="border p-4 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20 shadow-xl overflow-hidden">
+                <table class="w-full overflow-x-auto"> 
                     <tr class="bg-[#FEE715]">
                         <th></th>
                         <th class="py-3">Image</th>
@@ -70,8 +70,8 @@
     </section>
 
     <section>
-        <div class="w-5/6 mx-auto flex justify-end pb-5">
-            <div class="w-2/5 uppercase px-4 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20 shadow-lg border-t">
+        <div class="w-11/12 md:w-5/6 mx-auto flex justify-end pb-5">
+            <div class="w-full md:w-2/5 uppercase px-4 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20 shadow-lg border-t">
                 <div class="flex justify-between py-4 text-xl font-semibold border-b">
                     <span>Subtotal</span>
                     <span>&#8358;26,400.00</span>
@@ -86,7 +86,7 @@
                 </div>
             </div>                    
         </div>
-        <div class="flex justify-end w-5/6 mx-auto py-4">
+        <div class="flex justify-end w-11/12 md:w-5/6 mx-auto py-4">
             <a href="{{ route('checkout') }}" class="px-6 py-2 bg-[#FEE715] rounded-md font-semibold text-lg uppercase">Proceed to checkout</a>
         </div>  
     </section>
