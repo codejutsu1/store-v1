5fb546a786590d3c8b78367e6ca2bba7b3cf47e0
codejutsu1 <codejutsu@protonmail.com>
2023-06-29 07:42:13 +0100

    made the review section and feed section responive and also move css and js to thier respective folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,92 @@ import.meta.glob([
     '../images/**',
     '../fonts/**',
   ]);
+
+var $typed_strings = document.getElementById('typed-strings');
+var $typed_strings2 = document.getElementById('typed-strings2');
+
+if($typed_strings || $typed_strings2)
+{
+    var typed = new Typed('#typed-strings', {
+        strings: ['My Gee!!!'],
+        typeSpeed: 150,
+        showCursor: false,
+        loop: true,
+        backDelay: 14000,
+    });
+
+    var typed = new Typed('#typed-strings2', {
+        strings: [
+            'You dey find the best shopping plug' + String.fromCodePoint(0x1F601) + '?',
+            'Na here we dey' + String.fromCodePoint(0x1F60E) + '.'
+            ],
+        typeSpeed: 80,
+        startDelay: 3000,
+        loop: true,
+        backDelay: 3000,
+        backspeed: 5000,
+        showCursor: false,
+    });
+}
+
+const swiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    loop: true, 
+    slidesPerView: 2,
+    spaceBetween: 50, 
+    centeredSlides: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        }
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+});
+
+const swiper2 = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    coverflowEffect: {
+        rotate: 0,
+        stetch: 0,
+        depth: 0,
+        modifier: 1,
+        slideshadows: true,
+    },
+
+    loop: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        400: {
+            slidesPerView: 2,
+        },
+
+        640: {
+            slidesPerView: 3,
+        }
+    },
+});
--- resources/views/components/page/feed-slider.blade.php
+++ resources/views/components/page/feed-slider.blade.php
@@ -1,23 +1,23 @@
 <div class="py-10 mx-auto">
-        <h1 class="text-center text-4xl uppercase font-semibold my-10">
+        <h1 class="text-center text-2xl md:text-4xl uppercase font-semibold my-10">
             Feeds
         </h1>
        <div class="swiper mySwiper">
             <div class="swiper-wrapper ">
                 <div class="swiper-slide">
-                    <img class="h-full w-full object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-1.jpg') }}" alt="A food Poster">
+                    <img class="h-full w-full object-fill md:object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-1.jpg') }}" alt="A food Poster" loading="lazy">
                 </div>
                 <div class="swiper-slide">
-                    <img class="h-full w-full object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-2.jpg') }}" alt="A food Poster">
+                    <img class="h-full w-full object-fill md:object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-2.jpg') }}" alt="A food Poster" loading="lazy">
                 </div>
                 <div class="swiper-slide">
-                    <img class="h-full w-full object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-3.jpg') }}" alt="A food Poster">
+                    <img class="h-full w-full object-fill md:object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-3.jpg') }}" alt="A food Poster" loading="lazy">
                 </div>
                 <div class="swiper-slide">
-                    <img class="h-full w-full object-cover" src="{{ Vite::asset('resources/images/feeds/food-poster-4.jpg') }}" alt="A food Poster">
+                    <img class="h-full w-full object-fill md:object-cover ob" src="{{ Vite::asset('resources/images/feeds/food-poster-4.jpg') }}" alt="A food Poster" loading="lazy">
                 </div>
             </div>
 
-            <div class="swiper-pagination"></div>
+            <div class="swiper-pagination text-[#FEE715]"></div>
        </div>
     </div>
