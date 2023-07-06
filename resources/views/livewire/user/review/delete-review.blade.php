6a0d6941aa62dc7da8d2bb60c32674746c33d428
codejutsu1 <codejutsu@protonmail.com>
2023-07-06 18:18:01 +0100

    created a delete component

--- resources/views/livewire/user/review/delete-review.blade.php
+++ resources/views/livewire/user/review/delete-review.blade.php
@@ -1,16 +1,1 @@
-<div class="bg-gray-900 p-10">
-    <div class="flex items-center justify-center mb-8">
-        <img class="w-20 h-20" src="{{ Vite::asset('resources/images/review/warning.svg') }}" alt="A warning sign">
-    </div>
-    <h1 class="text-xl text-center font-bold text-gray-200">
-        Are you sure you want to delete this Review?
-    </h1>
-    <div class="flex justify-between pt-10">
-        <button wire:click="destroy" class="px-4 py-2 bg-red-700 text-red-100 rounded-md font-semibold">
-            Yes, delete it!
-        </button>
-        <button wire:click="$emit('closeModal')" class="px-8 py-2 bg-zinc-700 text-zinc-100 rounded-md font-semibold">
-            No
-        </button>
-    </div>
-</div>
+<x-delete title="review" />
