23fd322ed44d79503c062dfba819721aeb3fac59
codejutsu1 <codejutsu@protonmail.com>
2023-07-12 22:20:01 +0100

    designed and developed the create product form

--- app/Http/Livewire/User/Product/CreateProduct.php
+++ app/Http/Livewire/User/Product/CreateProduct.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Livewire\User\Product;
 
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class CreateProduct extends ModalComponent
 {
+    use WithFileUploads;
+    
+    public $name, $original_price, $image, $description;
+
+    protected $rules = [
+        'name' => 'required',
+        'original_price' => 'required|number',
+        'image' => 'required|mimes:jpeg,jpg,png|max:200',
+        'description' => 'required',
+    ];
+
     public function render()
     {
         return view('livewire.user.product.create-product');
--- resources/views/livewire/user/product/create-product.blade.php
+++ resources/views/livewire/user/product/create-product.blade.php
@@ -1,3 +1,31 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="p-8 bg-gray-900">
+        <div>
+            <h1 class="text-2xl text-center text-gray-200 font-semibold">Create a new Product</h1>
+            <form wire:submit.prevent="submit" class="mt-5 text-gray-200">
+                <div>
+                    <input type="text" wire:model.debounce.500ms="name" class="block w-full bg-gray-800 focus:ring-0 focus:border-gray-200 rounded-md" placeholder="Name" autofocus>
+                    @error('name') <span class="text-red-600 font-bold">{{ $message }}</span> @enderror
+                </div>
+                <div>
+                    <div class="flex rounded-md border mt-10 pl-1">
+                        <span class="bg-gray-900 flex items-center justify-center p-2 font-semibold">&#8358;</span>
+                        <input type="tel" wire:model.debounce.500ms="original_price" class="block w-full bg-gray-800 focus:ring-0 focus:border-gray-200 border-0 rounded-md" placeholder="Price of product">
+                    </div>
+                    @error('original_price') <span class="text-red-600 font-bold">{{ $message }}</span> @enderror
+                </div>
+                <div>
+                    <x-file-pond wire:model="image" class="mt-10" />
+                    @error('image')<span class="text-red-600 font-bold">{{ $message }}</span> @enderror
+                </div>
+                <div>
+                    <textarea wire:model.debounce.500ms="description" class="block w-full bg-gray-800 focus:ring-0 focus:border-gray-200 rounded-md mt-10" rows="5" placeholder="Image Description...."></textarea>
+                    @error('description') <span class="text-red-600 font-bold">{{ $message }}</span> @enderror
+                </div>
+                <div class="flex justify-end mt-5">
+                    <input type="submit" class="bg-purple-600 cursor-pointer text-gray-200 px-5 py-2 rounded-md text-lg font-semibold" value="Create" />
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
