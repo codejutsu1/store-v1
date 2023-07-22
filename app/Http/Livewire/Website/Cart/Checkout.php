26a3b68b39ecb0371396fe06be49f58e07306457
codejutsu1 <codejutsu@protonmail.com>
2023-07-22 08:53:53 +0100

    validated the billing address input

--- app/Http/Livewire/Website/Cart/Checkout.php
+++ app/Http/Livewire/Website/Cart/Checkout.php
@@ -9,9 +9,29 @@ class Checkout extends Component
 {
     public $cart_subtotal;
     
-    public $first_name, $last_name, $email, $tel, $lodge, $additional_information;
+    public $first_name, $last_name, $email, $tel, $lodge, $additional_information, $area="";
 
-    public $area = '';
+    protected $rules = [
+        'first_name' => 'required|string',
+        'last_name' => 'required|string',
+        'email' => 'required|string|email',
+        'tel' => 'required|string|numeric',
+        'lodge' => 'required|string',
+        'area' => 'required|string',
+        'additional_information' => 'required|string'
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        dd($this->validate());
+    }
 
     public function render()
     {
