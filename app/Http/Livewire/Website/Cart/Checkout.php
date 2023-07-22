365dec5290b1413f2fb46850d541f7c1d3867b66
codejutsu1 <codejutsu@protonmail.com>
2023-07-22 09:54:51 +0100

    able to process payment in paystack and added a try and catch to catch errors in the payment process

--- app/Http/Livewire/Website/Cart/Checkout.php
+++ app/Http/Livewire/Website/Cart/Checkout.php
@@ -4,12 +4,11 @@ namespace App\Http\Livewire\Website\Cart;
 
 use Livewire\Component;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Paystack;
 
 class Checkout extends Component
 {
-    public $cart_subtotal;
-    
-    public $first_name, $last_name, $email, $tel, $lodge, $additional_information, $area="";
+    public $cart_subtotal, $first_name, $last_name, $email, $tel, $lodge, $additional_information, $area="";
 
     protected $rules = [
         'first_name' => 'required|string',
@@ -28,9 +27,28 @@ class Checkout extends Component
 
     public function submit()
     {
-        $this->validate();
+        $validated_data = $this->validate();
 
-        dd($this->validate());
+        $amount = floatval(Cart::subtotal()) * 1000;
+        
+        $data = array(
+            "amount" => $amount * 100,
+            "reference" => paystack()->genTranxRef(),
+            "email" => $validated_data['email'],
+            "currency" => "NGN",
+            "metadata" => [
+                "cart_content" => Cart::content(),
+                "billing_address" => $validated_data,
+            ]
+        );
+
+        try{
+            return Paystack::getAuthorizationUrl($data)->redirectNow();
+        }catch(\Exception $e) {
+            toast('The paystack token has expired. Please refresh the page and try again.', 'error');
+
+            return redirect()->route('checkout');
+        } 
     }
 
     public function render()
