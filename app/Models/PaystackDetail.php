8dc714ba55e4ee8590c797996d06fac9d51a7274
codejutsu1 <codejutsu@protonmail.com>
2023-07-23 15:46:55 +0100

    made the paystack details fillable

--- app/Models/PaystackDetail.php
+++ app/Models/PaystackDetail.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class PaystackDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'id',
+        'domain',
+        'status',
+        'reference',
+        'amount',
+        'gateway_response',
+        'ip_address',
+        'fees',
+        'authorization_code',
+        'brand',
+        'card_type',
+        'bank',
+    ];
 }
