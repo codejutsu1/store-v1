62d4e1002e920bf328c794d86fee20ce74e59926
codejutsu1 <codejutsu@protonmail.com>
2023-08-11 22:51:05 +0100

    made the radio button redirect to it's category link

--- app/Http/Livewire/Website/Category/RadioButton.php
+++ app/Http/Livewire/Website/Category/RadioButton.php
@@ -3,11 +3,31 @@
 namespace App\Http\Livewire\Website\Category;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class RadioButton extends Component
 {
+    public $categories;
+    public $category_url;
+
+    public function mount()
+    {
+        $this->categories = Category::get(['id', 'name', 'slug']);
+    }
+
+    public function redirectCategory($category_id)
+    {
+        $category_slug = Category::where('id', $category_id)->value('slug');
+
+        return redirect()->route('shop', $category_slug);
+    }
+
     public function render()
     {
+        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        $this->category_url = basename(parse_url($url, PHP_URL_PATH));
+
         return view('livewire.website.category.radio-button');
     }
 }
--- resources/views/livewire/website/category/radio-button.blade.php
+++ resources/views/livewire/website/category/radio-button.blade.php
@@ -1,12 +1,20 @@
 <div>
     <form class="flex justify-between">
+        @forelse($categories as $category)
         <div>
-            <label for="category1" class="md:flex items-center px-2 category">
-                <input type="radio" name="catergories" id="category1"  class="w-12 h-6 outline-none bg-gray-200 cursor-pointer rounded-[20px] relative" checked>
-                <span class="block p-2 md:p-5 category-span font-semibold text-gray-200">
-                    Category 1
-                </span>
+            <label for="{{ $category->name }}" class="md:flex items-center px-2 category">
+                <input type="radio" name="categories"  wire:click="redirectCategory({{ $category->id }})" id="{{ $category->name }}"  class="w-12 h-6 outline-none bg-gray-200 cursor-pointer rounded-[20px] relative"
+                    {{ $category->slug == $category_url ? 'checked' : '' }}
+                />
+                <a href="{{ route('shop', $category->slug) }}">
+                    <span class="block p-2 md:p-5 category-span font-semibold text-gray-200">
+                        {{ $category->name }}
+                    </span>
+                </a>
             </label>
         </div>
+            @empty
+            <p class="text-center font-semibold">There is no category yet...</p>
+        @endforelse
     </form>
 </div>
